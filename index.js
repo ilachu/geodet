08c58db39842ffaa04007a5cae7e77045f250e1a
lakshminarayanan sv <lakshminarayanan2060@gmail.com>
2015-11-30 13:35:24 +0530

    v2.0.0 Vincenty's Formulae ( WGS'84 - Ellipsoid )

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 var wgsMod = {};
-Number.prototype.toRad = function() {
-	return this*3.14/180;
+Number.prototype.pow = function( n ) {
+	return Math.pow( this , n );
 };
-function hav( theta ) { //haversine function
-	return Math.pow( Math.sin( theta )/2 , 2 );
+Number.prototype.toRad = function (){
+  return ( this * 3.14 ) / 180;
+};
+function sin( theta ) {  
+	return Math.sin( theta );
 }
+function sin2( theta ) {
+	return sin( theta ).pow( 2 );
+}
+function cos( theta ) {
+	return Math.cos( theta );
+}
+function cos2( theta ){
+	return cos( theta ).pow( 2 );
+} //Math functions - better readability
 
-wgsMod.coordDistance = function ( coords ) {  //haversine formula
+wgsMod.coordDistance = function ( coords ) {  //Vincenty's formulae
 	var 
-		R = 6371, //radius of Earth
 		lat1 = coords.lat1.toRad(),
-		lat2 = coords.lat2.toRad(),
 		lng1 = coords.lng1.toRad(),
-		lng2 = coords.lng2.toRad(),
-		deltaLat = lat2 - lat1,
-		deltaLng = lng2 - lng1,
-		h = hav( deltaLat ) + Math.cos( lat1 )*Math.cos( lat2 )*hav( deltaLng );
-	return 2*R*Math.asin( Math.sqrt( h ) );
+		lat2 = coords.lat2.toRad(),
+		lng2 = coords.lng2.toRad(), //co-ordinates in degrees
+		//CONSTANTS - Ellipsoidal Earth
+		f = 1/298.257223563, //inverse flattening
+		a = 6378137.0, //semi major axis
+		b = 6356752.314245, //semi minor axis
+		L = lng2 - lng1,
+		U1 = Math.atan( ( 1-f ) * Math.tan( lat1 ) ),
+		U2 = Math.atan( ( 1-f ) * Math.tan( lat2 ) ), //reduced latitudes
+		lambda = L, //initially
+		lambdaP = Math.PI * 2,
+		limit = 20; //while loop
+
+		do { //iterate until L converges to 1e-12
+			sinSigma = Math.sqrt( (cos( U2 ) * sin( lambda )).pow( 2 ) + 
+				( cos( U1 ) * sin( U2 ) - sin( U1 ) * cos( U2 ) * cos( lambda ) ).pow( 2 ) );
+			if (!sinSigma) return 0; //co-incident points
+			cosSigma = sin( U1 ) * sin( U2 ) + cos( U1 ) * cos( U2 ) * cos( lambda );
+			sigma = Math.atan( sinSigma / cosSigma );
+			sinAlpha = ( cos( U1 ) * cos( U2 ) * sin( lambda ) ) / sin ( sigma );
+			cos2Alpha = 1 - sinAlpha.pow( 2 );
+			cosTwoSigmaBaseM = cosSigma - ( ( 2 * sin( U1 ) * sin( U2 ) ) / cos2Alpha );
+			C = ( f / 16 ) * cos2Alpha * ( 4 + f * ( 4 - 3 * cos2Alpha ));
+			lambdaP = lambda;
+			lambda =  L + ( 1 - C ) * f * sinAlpha * 
+				( sigma + C * sinSigma * ( cosTwoSigmaBaseM + C * cosSigma * ( -1 + 2 * cosTwoSigmaBaseM ) ) );
+		} while( Math.abs( lambda - lambdaP ) > 1e-12 && limit-->0) ;
+
+		uSq = cos2Alpha * ( ( a.pow( 2 ) - b.pow( 2 ) ) / b.pow( 2 ) );
+		A = 1 + ( ( uSq / 16384 ) * ( 4096 + uSq * ( -768 + uSq * ( 320 - 175 * uSq ) ) ) );
+		B = uSq / 1024 * ( 256 + uSq * ( -128 + uSq * ( 74 - 47 * uSq ) ) );
+		deltaSigma = B * sinSigma * 
+			( cosTwoSigmaBaseM + 0.25 * B * ( cosSigma * ( -1 + 2 * cosTwoSigmaBaseM.pow( 2 ) )  - 
+			( 1 / 6 ) * B * cosTwoSigmaBaseM * ( -3 + 4 * sinAlpha.pow( 2 ) ) * 
+			( -3 + 4 * cosTwoSigmaBaseM ) ) );
+		return b * A * ( sigma - deltaSigma );
 };
 
 module.exports = wgsMod;
--- test/test.js
+++ test/test.js
@@ -2,14 +2,32 @@ var assert = require('assert');
 var wgsMod = require('../index.js');
 describe( 'wgsMod',function () {
 	describe( '#coordDistance',function () {
-		it( 'calculates distance between given co-ordinates',function () {
+		it( 'calculates distance between given co-ordinates #1',function () {
 			coords = {
 				lat1 : 13.0475255,
 				lng1 : 80.2090117,
 				lat2 : 13.0575255,
 				lng2 : 80.2090117
 			};
-			assert(wgsMod.coordDistance( coords ),1.111389095);
+			assert.equal(wgsMod.coordDistance( coords ), 1105.747919147229 );
 		} );
+		it( 'calculates distance between given co-ordinates #2', function () {
+			coords = {
+				lat1 : 12.9753583,
+				lng1 : 79.1604861,
+				lat2 : 12.9663408,
+				lng2 : 79.1679373
+			};
+			assert.equal( wgsMod.coordDistance( coords ) , 1283.4144216524535 );
+		} );
+		it( 'returns distance as zero for co-incident points' , function () {
+			coords = {
+				lat1 : 13.0475255,
+				lng1 : 80.2090117,
+				lat2 : 13.0475255,
+				lng2 : 80.2090117
+			};
+			assert.equal(wgsMod.coordDistance( coords ) , 0 );
+		} )
 	} );
 } );
